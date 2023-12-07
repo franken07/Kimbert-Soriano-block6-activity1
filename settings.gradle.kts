523f3c8b0af6f9d95c29deb37a8c00e3fd7ded75
Kimbert Soriano <sorianokimbertmark@gmail.com>
2023-12-07 15:56:02 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "com.soriano.kimbert.block6.p1.quiz"
+include(":app")
